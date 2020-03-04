5ac5ec45e544269e0e46bf6901f0aedda3cda45a
William Webb <webbdevvue@gmail.com>
2020-03-03 19:58:10 -0500

    mvp

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,18 +1,23 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
-// import data 
+// import data
 
-const PostsPage = () => {
-  // set up state for your data
+
+const PostsPage = (props) => {
+  //set up state for your data
+
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      {props.dummyData.map((item) => {
+
+        return <Post post={item}/>;
+      })}
     </div>
   );
 };
 
 export default PostsPage;
-
